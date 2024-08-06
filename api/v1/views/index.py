8e08a16a8b7f529c7e091a262857301dcdcf8f69
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-08-06 14:37:18 -0700

    Definte route reporting number of each object by type.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 # Route definitions
@@ -12,3 +13,17 @@ from flask import jsonify
 def get_status():
     """Return a JSON response describing the Web app status."""
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats", strict_slashes=False)
+def get_stats():
+    """Return the number of each objects by type."""
+    obj_list = list(storage.all().values())
+    report = {}
+
+    for obj in obj_list:
+        if obj.__class__.__name__ in report:
+            report[obj.__class__.__name__] += 1
+        else:
+            report[obj.__class__.__name__] = 1
+
+    return (jsonify(report))
