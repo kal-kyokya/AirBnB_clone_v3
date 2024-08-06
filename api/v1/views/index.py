fe7c58e987d462461fd9a8dd205e8181c47d1e2e
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-08-06 15:08:59 -0700

    Integrate custom JSON response for 404 Errors.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,13 +9,13 @@ from models import storage
 
 # Route definitions
 
-@app_views.route("/status", strict_slashes=False)
+@app_views.route("/status")
 def get_status():
     """Return a JSON response describing the Web app status."""
     return jsonify({"status": "OK"})
 
 
-@app_views.route("/stats", strict_slashes=False)
+@app_views.route("/stats")
 def get_stats():
     """Return the number of each objects by type."""
     obj_list = list(storage.all().values())
