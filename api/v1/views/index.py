7f052a0a35eed8a324988018d02aa718a6cdccb0
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-08-07 05:53:38 -0700

    Deb 'no documentation error'.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""
-'index.py' contains route defintions for the app_views blueprint.
+"""index file, main view file
 """
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
-
-# Route definitions
 
 @app_views.route("/status")
 def get_status():
@@ -18,11 +15,11 @@ def get_status():
 @app_views.route("/stats")
 def get_stats():
     """Return the number of each objects by type."""
-     report = {"amenities": storage.count(amenities),
-               "cities": storage.count(cities),
-               "places": storage.count(places),
-               "reviews": storage.count(reviews),
-               "states": storage.count(states),
-               "users": storage.count(users)}
+    report = {"amenities": storage.count(amenities),
+              "cities": storage.count(cities),
+              "places": storage.count(places),
+              "reviews": storage.count(reviews),
+              "states": storage.count(states),
+              "users": storage.count(users)}
 
     return (jsonify(report))
