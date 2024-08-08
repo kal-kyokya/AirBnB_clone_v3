bb6999d4023e1f2e09bab75679259fa69af9bc10
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-08-08 08:34:45 -0700

    Integrate Password hashing.

--- models/base_model.py
+++ models/base_model.py
@@ -21,7 +21,6 @@ else:
 
 class BaseModel:
     """The BaseModel class from which future classes will be derived"""
-
     if models.storage_t == "db":
         id = Column(String(60), primary_key=True)
         created_at = Column(DateTime, default=datetime.utcnow)
@@ -59,7 +58,7 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self):
+    def to_dict(self, save_check=False):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
@@ -69,6 +68,8 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+        if "password" in new_dict and save_check is False:
+            del new_dict["password"]
         return new_dict
 
     def delete(self):
